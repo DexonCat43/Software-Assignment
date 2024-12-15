272de57fdec3e65248d4628dd2e4a6fec500e113
DexonCat43 <aguila.leon@education.nsw.gov.au>
2024-12-16 09:22:39 +1100

    Update setup.sql
    moviesentral setup db

--- database/setup.sql
+++ database/setup.sql
@@ -1,15 +1,22 @@
---CREATE TABLE IF NOT EXISTS users(
---    id INTEGER PRIMARY KEY AUTOINCREMENT,
---    username TEXT UNIQUE NOT NULL,
---    password TEXT NOT NULL
---);
+-- Drop old tables if they exist
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS reviews;
 
-CREATE TABLE IF NOT EXISTS entries (
+-- Create the users table
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+);
+
+-- Create the reviews table
+CREATE TABLE reviews (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
-    title TEXT NOT NULL,
-    description TEXT,
-    image_path TEXT NOT NULL,
+    movie_title TEXT NOT NULL,
+    review TEXT NOT NULL,
+    rating INTEGER NOT NULL,
+    poster_path TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) references users (id)
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
